58c5caae51d0e4a0ae6281120004759347efd59c
meat-raffle <hannahlalexander@gmail.com>
2022-03-24 14:45:26 +1100

    re-wrote js day & date

--- src/script.js
+++ src/script.js
@@ -1,47 +1,43 @@
-let current = new Date();
-let date = current.getDate();
-function formatDate() {
-  let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
-  let day = days[current.getDay()];
-  let months = [
-    "Jan",
-    "Feb",
-    "March",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sept",
-    "Oct",
-    "Nov",
-    "Dec"
-  ];
-  let month = months[current.getMonth()];
-  let displayCurrentDate = document.querySelector("#current-day");
-  displayCurrentDate.innerHTML = `${date} ${month}, ${day}`;
-}
-formatDate();
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minute = current.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
 
-let hour = current.getHours();
-if (hour < 10) {
-  hour = `0${hour}`;
+    let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+    let day = days[current.getDay()];
+    return `${date} ${month}, ${day}`;
+  }
+  let displayCurrentTime = document.querySelector("#current-time");
+  displayCurrentTime.innerHTML = `${hour}:${minute}`;
 }
-let minute = current.getMinutes();
-if (minute < 10) {
-  minute = `0${minute}`;
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = day.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
 }
-let displayCurrentTime = document.querySelector("#current-time");
-displayCurrentTime.innerHTML = `${hour}:${minute}`;
 
 function displayWeather(response) {
+  console.log(response.data);
   let forecast = Math.round(response.data.main.temp);
+  let iconElement = document.querySelectorAll("icon");
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = `${forecast}°C`;
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
